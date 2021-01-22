0eabae8d1a71010716ae278001ee8215e0514696
Ash Searle <ash@hexmen.com>
2021-01-22 12:07:54 +0000

    Add chakra-ui deps, reset styles, config import paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,38 @@
-import { AuthProvider } from "../lib/auth";
-import "../styles/globals.css";
+import { ThemeProvider, CSSReset } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import { AuthProvider } from "@/lib/auth";
+
+const customTheme = {};
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+    </>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
