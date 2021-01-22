17de9f7bde63eefead8f15287561a97416d12f21
Ash Searle <ash@hexmen.com>
2021-01-22 12:07:54 +0000

    Add chakra-ui deps, reset styles, config import paths

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import Head from 'next/head'
-import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useAuth } from "@/lib/auth";
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>R2025</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          r2025
-        </h1>
+      <main>
+        <h1>r2025</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
-        {!auth?.user && <button onClick={e => auth.signinWithGitHub()}>Sign In</button>}
+        {!auth?.user && (
+          <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
+        )}
         <div>{auth?.user?.email}</div>
-        {auth?.user && <button onClick={e => auth.signout()}>Sign Out</button>}
+        {auth?.user && (
+          <button onClick={(e) => auth.signout()}>Sign Out</button>
+        )}
       </main>
     </div>
-  )
+  );
 }
