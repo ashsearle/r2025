be16f9b871b928022f2ac053111c8d9a46a77195
Ash Searle <ash@hexmen.com>
2021-01-25 10:11:14 +0000

    Simplify home page

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import Head from "next/head";
-import { Button, Heading, Text, Code } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import { useAuth } from "@/lib/auth";
+import { LogoIcon } from "@/styles/icons";
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div>
+    <Flex
+      as="main"
+      direction="column"
+      w="full"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
       <Head>
         <title>R2025</title>
       </Head>
 
-      <main>
-        <Heading>r2025</Heading>
-        <Text>
-          Current user: <Code>{auth?.user?.email ?? "None"}</Code>
-        </Text>
+      <LogoIcon color="black" boxSize="48px" />
 
-        {auth.user ? (
-          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      {auth.user ? (
+        <Button onClick={(e) => auth.signout()} mt={4} size="sm">
+          Sign Out
+        </Button>
+      ) : (
+        <Button onClick={(e) => auth.signinWithGitHub()} mt={4} size="sm">
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 }
