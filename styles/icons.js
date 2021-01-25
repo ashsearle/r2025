55bf6e0ca0d06fd8eab5e4b58b5b31d6e1c5ebca
Ash Searle <ash@hexmen.com>
2021-01-25 10:11:14 +0000

    Simplify home page

---
+++ styles/icons.js
@@ -0,0 +1,19 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const LogoIcon = createIcon({
+  path: (
+    <g strokeWidth="6" fill="none" stroke="currentColor">
+      <polyline points="-6.93,20.00 0.00,24.00 6.93,20.00"></polyline>
+      <polyline points="13.86,16.00 20.78,12.00 20.78,4.00"></polyline>
+      <line x1="6.93" y1="-4.00" x2="20.78" y2="-12.00"></line>
+      <circle r="10"></circle>
+      <polyline points="20.78,-4.00 20.78,-12.00 13.86,-16.00"></polyline>
+      <polyline points="6.93,-20.00 0.00,-24.00 -6.93,-20.00"></polyline>
+      <polyline points="-13.86,-16.00 -20.78,-12.00 -20.78,-4.00"></polyline>
+      <polyline points="-20.78,4.00 -20.78,12.00 -13.86,16.00"></polyline>
+    </g>
+  ),
+  viewBox: "-34 -34 68 68",
+});
+
+
